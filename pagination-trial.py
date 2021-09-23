effb3598e400a9dccfc4f5468420c10adf206fe0
Yoon-Choi <choi.yoonjae@ewhain.net>
2021-09-24 01:31:46 +0200

    pagination implemented

--- pagination-trial.py
+++ pagination-trial.py
@@ -1,34 +1,38 @@
-
-#https://en.zalando.de/clothing/
-
-# brand_name, product_name, price
+#https://en.zalando.de/premium-men-clothing/?p=2
+#https://en.zalando.de/premium-men-clothing
+# brand, product, price
 
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
+# useragent and request from webpage
+hdr = {
+    'User-Agent': 'Mozilla/5.0(WindowsNT10.0;Win64;x64)AppleWebKit/537.36(KHTML,like Gecko)Chrome/92.0.4515.159Safari/537.36'}
+html = requests.get('https://en.zalando.de/premium-men-clothing', headers=hdr)
 
+# response checking
+print("webpage response : ", html.status_code)
+
+# pagination : this website is consist of 91 pages
 search_result = []
-
-
-
-for x in range(1, 3):  # pagination
-    url = 'https://www.glassdoor.com/Job/berlin-junior-data-analyst-jobs-SRCH_IL.0,6_IC2622109_KO7,26.htm?includeNoSalaryJobs=true&p='
-    r = requests.get(url+str(x)) # page넘겨가면서 html끝에 페이지번호가 보이는것을 확인 후에
+for x in range(1, 91):
+    url = 'https://en.zalando.de/premium-men-clothing/?p='
+    r = requests.get(url+str(x))
     soup = BeautifulSoup(r.content, 'html.parser')
-    content = soup.find_all('div', class_ =  "d-flex flex-column pl-sm css-1d3xmk8 e1rrn5ka4")
+    content = soup.find_all('div', class_="_0xLoFW _78xIQ- EJ4MLB JT3_zV")
     print(content)
     for property in content:
-        company = property.find('div', class_='d-flex justify-content-between align-items-start').text
-        position = property.find('a', class_='jobLink css-1rd3saf eigr9kq2').text
-        location = property.find('span', class_='pr-xxsm css-1ndif2q e1rrn5ka0').text
-        job_age = property.find('div', class_='d-flex align-items-end pl-std css-mi55ob').text
+        brand = property.find('span', class_='u-6V88 ka2E9k uMhVZi FxZV-M Kq1JPK pVrzNP ZkIJC- r9BRio qXofat EKabf7 nBq1-s _2MyPg2').text
+        product = property.find('h3', class_='u-6V88 ka2E9k uMhVZi FxZV-M _6yVObe pVrzNP ZkIJC- r9BRio qXofat EKabf7 nBq1-s _2MyPg2').text
+        price = property.find('span', class_='u-6V88 ka2E9k uMhVZi FxZV-M _6yVObe pVrzNP cMfkVL').text
+
         property_info = {
-            'company' : company,
-            'position': position,
-            'location' : location,
-            'job_age' : job_age}
+            'brand': brand,
+            'product': product,
+            'price': price
+            }
         search_result.append(property_info)
 print('search_result found : ', len(search_result))
 time.sleep(3)
